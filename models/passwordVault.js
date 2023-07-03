adba83e89d83279f51dd327ed7e2565920a0bc3d
rxn3202 <126435652+rxn3202@users.noreply.github.com>
2023-07-03 13:11:07 -0700

    added logic for passwords in models

---
+++ models/passwordVault.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class PasswordVault extends Model {}
+
+PasswordVault.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        website: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'passwordvault',
+    }
+);
+
+module.exports = PasswordVault;
